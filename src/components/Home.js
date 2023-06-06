a32e6b39dd7c809bbad5adb91318304f14300902
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-07 00:27:00 +0100

    Include Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+const Home = () => (
+  <div className="home-container">
+    <h2> WelCome to Our Page</h2>
+    <p>
+      We are here to unlock the world of arithmetic for you and provide a dose of
+      math-inspired inspiration. How cool is that? 😉 Brace yourself for some magical
+      math moments as we dive into the realm of numbers and equations. Let&rsquo;s ignite the
+      spark of curiosity and embark on an extraordinary journey
+      of mathematical wonders!✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨✨.
+
+    </p>
+  </div>
+);
+export default Home;
